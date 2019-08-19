288b3d87716b7eb8179b4ecbc39bf69d3a1b37eb
chengderen <38171858+chengderen@users.noreply.github.com>
2019-08-19 21:32:35 +0800

    chengderen

---
+++ src/Smartflow/IWorkflowAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smartflow
+{
+    public interface IWorkflowAction
+    {
+        /// <summary>
+        /// 执行跳转前的操作
+        /// </summary>
+        void ActionExecute();
+
+
+        /// <summary>
+        /// 执行跳转后的操作
+        /// </summary>
+        void ActionExecuted();
+    }
+}
